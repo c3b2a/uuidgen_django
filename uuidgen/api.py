aebf3e0ff27ef6171c506ddf8bd8dc3503b37fb5
nothing-blog <admin@nothingblog.top>
2020-08-12 20:29:06 +0800

    code format update(useless)

--- uuidgen/api.py
+++ uuidgen/api.py
@@ -1,22 +1,22 @@
 from django.http import HttpResponse
 
-import uuid,string,random
+import uuid, string, random
 
 def random_string(length):
-    ran_str = ''.join(random.sample(string.ascii_letters + string.digits,length))
+    ran_str = ''.join(random.sample(string.ascii_letters + string.digits, length))
     return ran_str
 
 def process(req):
-	get = req.GET;
+	get = req.GET
 	if 'version' in get:
 		if get['version'] == '1':
 			return HttpResponse(uuid.uuid1())
 		elif get['version'] == '3':
-			return HttpResponse(uuid.uuid3(uuid.NAMESPACE_DNS,random_string(32)))
+			return HttpResponse(uuid.uuid3(uuid.NAMESPACE_DNS, random_string(32)))
 		elif get['version'] == '4':
 			return HttpResponse(uuid.uuid4())
 		elif get['version'] == '5':
-			return HttpResponse(uuid.uuid5(uuid.NAMESPACE_DNS,random_string(32)))
+			return HttpResponse(uuid.uuid5(uuid.NAMESPACE_DNS, random_string(32)))
 		else:
 			return HttpResponse('')
 	else:
--- uuidgen/views.py
+++ uuidgen/views.py
@@ -1,26 +1,26 @@
-from django.shortcuts import render,HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect
 from django.http import HttpResponse
 from django.views.decorators import csrf
 
-import uuid,random,string
+import uuid, random, string
 
 def random_str(length):
-    ran_str = ''.join(random.sample(string.ascii_letters + string.digits,length))
+    ran_str = ''.join(random.sample(string.ascii_letters + string.digits, length))
     return ran_str
 
 def display(req):
     if req.method == "GET":
-        return render(req,"index.html",{'uuid': uuid.uuid3(uuid.NAMESPACE_DNS,random_str(32)),'List': [False,False,True,False,False]})
+        return render(req, "index.html", {'uuid': uuid.uuid3(uuid.NAMESPACE_DNS, random_str(32)), 'List': [False, False, True, False, False]})
     elif req.method == "POST":
         if 'mode_select' in req.POST:
             if req.POST['mode_select'] == 'uuid1':
-                return render(req,"index.html",{'uuid': uuid.uuid1(),'List': [True,False,False,False,False]})
+                return render(req, "index.html", {'uuid': uuid.uuid1(), 'List': [True, False, False, False, False]})
             elif req.POST['mode_select'] == 'uuid3':
-                return render(req,'index.html',{'uuid': uuid.uuid3(uuid.NAMESPACE_DNS,random_str(32)),'List': [False,False,True,False,False]})
+                return render(req, 'index.html', {'uuid': uuid.uuid3(uuid.NAMESPACE_DNS, random_str(32)), 'List': [False, False, True, False, False]})
             elif req.POST['mode_select'] == 'uuid4':
-                return render(req,'index.html',{'uuid': uuid.uuid4(),'List': [False,False,False,True,False]})
+                return render(req, 'index.html', {'uuid': uuid.uuid4(), 'List': [False, False, False, True, False]})
             elif req.POST['mode_select'] == 'uuid5':
-                return render(req,'index.html',{'uuid': uuid.uuid5(uuid.NAMESPACE_DNS,random_str(32)),'List': [False,False,False,False,True]})
+                return render(req, 'index.html', {'uuid': uuid.uuid5(uuid.NAMESPACE_DNS, random_str(32)), 'List': [False, False, False, False, True]})
             else:
                 return HttpResponseRedirect('/')
         else:
